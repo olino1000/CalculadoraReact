e5cf5b5050282cb5315e08e5998b2d26495c722a
Vinícius Olino <olino1000@gmail.com>
2021-04-19 19:04:28 -0300

    Commit 19/04

---
+++ src/components/Display.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    ImageBackground,
+} from 'react-native'
+
+const style = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        alignItems: 'flex-end'
+    },
+    displayValue: {
+        fontSize: 60,
+        color: '#fff'
+    }
+})
+
+export default props => {
+
+    return (
+        <SafeAreaView style={style.display}>
+            <Text style={style.displayValue} numberOfLines={1}>
+                {props.value}
+            </Text>
+        </SafeAreaView>
+    )
+}
